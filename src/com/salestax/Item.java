600e75562e6d1d4d478f0925534249efdd095567
Rahul Daspal <daspalrahul@gmail.com>
2015-08-06 11:22:16 +0530

    Item knows if sales and income tax apply on it or not

--- src/com/salestax/Item.java
+++ src/com/salestax/Item.java
@@ -5,13 +5,25 @@ public class Item {
 
     private int quantity;
     private double price;
+    private boolean isSalesTaxable;
+    private boolean isImportTaxApplicable;
 
-    public Item(int quantity, double price) {
+    public Item(int quantity, double price, boolean isSalesTaxable, boolean isImportTaxApplicable) {
         this.quantity = quantity;
         this.price = price;
+        this.isSalesTaxable = isSalesTaxable;
+        this.isImportTaxApplicable = isImportTaxApplicable;
     }
 
     public double totalPrice() {
         return quantity * price;
+    }
+
+    public boolean isSalesTaxable() {
+        return isSalesTaxable;
+    }
+
+    public boolean isImportTaxApplicable() {
+        return isImportTaxApplicable;
     }
 }
--- test/com/salestax/ItemTest.java
+++ test/com/salestax/ItemTest.java
@@ -10,8 +10,22 @@ public class ItemTest {
 
     @Test
     public void itemKnowsItsPrice() {
-        Item item = new Item(2, 3.5);
+        Item item = new Item(2, 3.5, true, true);
 
         assertEquals(7, item.totalPrice(), 0);
+    }
+
+    @Test
+    public void itemKnowsIfItsSalesTaxable() {
+        Item item = new Item(2, 3.5, true, true);
+
+        assertEquals(true, item.isSalesTaxable());
+    }
+
+    @Test
+    public void itemKnowsIfItsImportTaxable() {
+        Item item = new Item(2, 3.5, true, true);
+
+        assertEquals(true, item.isImportTaxApplicable());
     }
 }
